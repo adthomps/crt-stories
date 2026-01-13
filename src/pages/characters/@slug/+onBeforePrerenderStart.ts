665a9c0089de74485cd613a696a5807441674627
Adam <athompson2008@gmail.com>
2026-01-12 22:41:25 -0800

    Update +onBeforePrerenderStart.ts

--- src/pages/characters/@slug/+onBeforePrerenderStart.ts
+++ src/pages/characters/@slug/+onBeforePrerenderStart.ts
@@ -3,5 +3,7 @@ import { characters } from '../../../content';
 
 export function onBeforePrerenderStart() {
   const urls = characters.map((character) => `/characters/${character.slug}`);
-  return urls;
+  // Deduplicate URLs in case of accidental duplicates
+  const uniqueUrls = Array.from(new Set(urls));
+  return uniqueUrls;
 }
